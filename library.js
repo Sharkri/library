14a0b5e3a463d31956489336dc3cc8eab18fd9c3
Sharkri <sharkdude955@gmail.com>
2022-07-09 22:41:19 -0500

    refactored stuff

--- library.js
+++ library.js
@@ -4,8 +4,7 @@ const overlay = document.querySelector(".overlay");
 const author = document.querySelector("#author");
 const title = document.querySelector("#title");
 const pages = document.querySelector("#pages");
-const read = document.querySelector("#read");
-overlay.style.display = "none";
+const readInput = document.querySelector("#read");
 let counter = 0;
 class Book {
   constructor(author, title, pages, read) {
@@ -31,16 +30,16 @@ function addBookToDisplay(book) {
   author.textContent += `Author: ${book.author} `;
   pages.textContent += `Pages: ${book.pages} `;
   read.textContent = book.read ? "Read" : "Not Read";
-  read.style.background = book.read ? "#4ade80" : "#f87171";
 
   remove.textContent = "Remove";
-
   read.classList.add(`read${counter}`);
+  if (read.textContent == "Read") read.classList.remove("not-read");
+  else read.classList.add("not-read");
+
   newDiv.classList.add(`newDiv${counter}`, "bookDivs");
   remove.classList.add(`remove${counter}`);
   read.setAttribute("id", "readButton");
   remove.setAttribute("id", "remove");
-
   // append stuff
   newDiv.appendChild(author);
   newDiv.appendChild(title);
@@ -50,65 +49,50 @@ function addBookToDisplay(book) {
   displayBooks.appendChild(newDiv);
   counter++;
 }
-function displayOnLoad() {
-  for (let book of myLibrary) {
-    addBookToDisplay(book);
-  }
-}
-displayOnLoad();
+const createDiv = () => document.createElement("div");
+
 document.addEventListener("click", (e) => {
   if (e.target.id === "remove") {
+    const bookNum = e.target.className.slice(-1);
     // Find which div to remove...
-    document.querySelector(`.newDiv${e.target.className.slice(-1)}`).remove();
+    document.querySelector(`.newDiv${bookNum}`).remove();
     // Remove from library
-    myLibrary.splice(e.target.className.slice(-1), 1);
+    myLibrary.splice(bookNum, 1);
     counter--;
     // Reorder index names.
     let divs = document.querySelectorAll(".bookDivs");
     for (let i = 0; i < divs.length; i++) {
       divs[i].classList = `newDiv${i} bookDivs`;
-      divs[i].querySelector("#readButton").classList = `read${i}`;
+      // Replace read class index with actual index
+      const readClasses = divs[i].querySelector("#readButton").classList;
+      readClasses.replace(readClasses[0], `read${i}`);
+
       divs[i].querySelector("#remove").classList = `remove${i}`;
     }
   }
   if (e.target.id === "readButton") {
-    let readText = document.querySelector(`.${e.target.className}`);
+    // Select the first class
+    const readBtn = document.querySelector(`.${e.target.classList[0]}`);
+    readBtn.classList.toggle("not-read");
 
-    let bool = readText.textContent === "Read" ? false : true;
-    if (bool) {
-      myLibrary[e.target.className.slice(-1)].read = true;
-      readText.textContent = "Read";
-      readText.style.background = "#4ade80";
-    } else {
-      myLibrary[e.target.className.slice(-1)].read = false;
-      readText.textContent = "Not Read";
-      readText.style.background = "#f87171";
-    }
+    const isRead = readBtn.className.includes("not-read") ? false : true;
+    const readNum = readBtn.classList[0].slice(-1);
+
+    myLibrary[readNum].read = isRead;
+    readBtn.textContent = isRead ? "Read" : "Not Read";
   }
 });
-function OverlayOpen() {
-  overlay.style.display = "flex";
-  overlay.style.content = " ";
-  overlay.style.width = "100%";
-  overlay.style.height = "100%";
-  overlay.style.position = "fixed";
-  overlay.style.top = "0";
-  overlay.style.left = "0";
-  overlay.style.background = "rgba(0, 0, 0, 0.5)";
+function openOverlay() {
+  overlay.classList.toggle("active");
   author.value = "";
   title.value = "";
   pages.value = "";
-  read.checked = false;
+  readInput.checked = false;
 }
-function closeOverlay() {
-  overlay.style.display = "none";
-}
+const closeOverlay = () => overlay.classList.toggle("active");
 function submitBook() {
   if (!author.value || !title.value || !pages.value) return;
   closeOverlay();
-  newBook = new Book(author.value, title.value, pages.value, read.checked);
+  newBook = new Book(author.value, title.value, pages.value, readInput.checked);
   addBookToLibrary(newBook);
-}
-function createDiv() {
-  return document.createElement("div");
 }
